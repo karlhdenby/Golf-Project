06db54987d7b9ec9781215c951d3b915dae4d273
karlhdenby <karlhdenby@gmail.com>
2024-10-24 22:48:29 -0700

    phase3"

---
+++ backend/db/migrations/20241025043201-create-user.js
@@ -0,0 +1,67 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA;
+}
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      username: {
+        type: Sequelize.STRING(20),
+        allowNull: false,
+        unique: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      phoneNumber: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        unique: true,
+      },
+      courseId: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      membership: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      employee: {
+        type: Sequelize.BOOLEAN,
+        allowNull: true,
+      },
+      rank: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      hashedPassword: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+    }, options);
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Users";
+    return queryInterface.dropTable(options);
+  },
+};
---
+++ backend/db/seeders/20241025045526-karl.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const { User } = require("../models");
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA;
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          username: "Karl",
+          email: "karlhdenby@gmail.com",
+          phoneNumber: "360-941-9691",
+          courseId: null,
+          employee: true,
+          rank: 4,
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+      ],
+      { validate: true }
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Users";
+    return queryInterface.bulkDelete(
+      options,
+      {
+        username: "Karl",
+      },
+      {}
+    );
+  },
+};
