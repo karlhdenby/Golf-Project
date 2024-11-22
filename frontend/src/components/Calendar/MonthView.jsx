b2029b416867453ac7e7464acda15afea3ba77c0
karlhdenby <karlhdenby@gmail.com>
2024-11-21 22:18:23 -0800

    calendar outline

--- frontend/src/components/Calendar/MonthView.jsx
+++ frontend/src/components/Calendar/MonthView.jsx
@@ -3,26 +3,43 @@ import './MonthView.css'
 
 export const MonthView = () => {
   const weeks = weekMaker();
+  const date = new Date().toString().split(" ");
+  const today = date[2]
+  console.log(today)
   
   return (
     <div className="days">
     <div className="week-days">
-        <p>M</p>
-        <p>T</p>
-        <p>W</p>
-        <p>T</p>
-        <p>F</p>
-        <p>S</p>
-        <p>S</p>
+        <div className="monday">
+            <p>Monday</p>
+        </div>
+        <div className="tuesday">
+            <p>Tuesday</p>
+        </div>
+        <div className="wednesday">
+            <p>Wednesday</p>
+        </div>
+        <div className="thursday">
+            <p>Thursday</p>
+        </div>
+        <div className="friday">
+            <p>Friday</p>
+        </div>
+        <div className="saturday">
+            <p>Saturday</p>
+        </div>
+        <div className="sunday">
+            <p>Sunday</p>
+        </div>
     </div>
     {weeks.map((week, weekIndex) => {
         return (
             <div className={`week-${weekIndex}`} key={weekIndex}>
             {week.map((day) => {
                 return(
-                    <p key={day} className={day}>
-                {day}
-                </p>
+                    <div key={day} className={day}>
+                        <p>{day}</p>
+                </div>
                 )
             })}
             </div>
